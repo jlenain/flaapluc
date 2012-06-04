f3274f52b98ecfe344f25279f72711e96bbc158e
fermi <fermi@96316d67-cb15-4e38-b937-3d89b22596c3>
2012-06-04 08:27:36 +0000

    Importing automaticLightCurve project into SVN repo.

---
+++ automaticLightCurve/automaticLightCurve.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#
+# @author Jean-Philippe Lenain, jplenain@lsw.uni-heidelberg.de
+# @version $Id$
+
+"""
+Automatic generation of photometric light curves of Fermi sources.
+"""
+
