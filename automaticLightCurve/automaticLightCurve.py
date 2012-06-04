52cbb56f89965569e03baf45760a9fccda3aa821
jplenain <jplenain@96316d67-cb15-4e38-b937-3d89b22596c3>
2012-06-04 09:43:23 +0000

    Added GTI filtering for a given source.

--- automaticLightCurve/automaticLightCurve.py
+++ automaticLightCurve/automaticLightCurve.py
@@ -12,13 +12,26 @@ import sys, os, asciidata
 from numpy import *
 
 # Flags
-DEBUG=True
+DEBUG=False
 
 
 
 class autoLC:
     def __init__(self,file="/home/jplenain/fermi/automaticLightCurve/listSources.txt"):
         self.file=file
+
+        # Import the Science Tools modules
+        try:
+            from gt_apps import *
+        except ImportError:
+            print "ERROR Can't import the Fermi Science tools"
+            sys.exit(1)
+
+        # Setting file names and directories
+        self.allsky     = "/data/fermi/allsky/allsky_30MeV_300GeV_diffuse_filtered.fits"
+        self.allskyGti  = "/data/fermi/allsky/allsky_30MeV_300GeV_diffuse_filtered_gti.fits"
+        self.spacecraft = "/data/fermi/allsky/allsky_SC00.fits"
+        self.workDir    = "/home/fermi/tmp"
 
     def readSourceList(self):
         try:
@@ -45,8 +58,49 @@ class autoLC:
                 print "DEBUG dec=",dec[i]
                 print "DEBUG z  =",z[i]
 
+        return zip(src,ra,dec,z)
 
-    def computePhotometricLC():
+
+    def selectSrc(src,ra,dec):
+        """
+        Filter a given source, running gtselect
+        """
+        filter['infile']=self.allskyGti
+        filter['outfile']=self.workDir+'/'+str(src)+'_gti.fits'
+        filter['ra']=ra
+        filter['dec']=dec
+        filter['rad']=10
+        filter['emin']=100.
+        filter['emax']=1.e5
+
+        # Open allsky file to get the start and stop dates
+        import pyfits
+        try:
+            hdu=pyfits.open(self.allskyGti)
+        except:
+            print 'Exception: can not open file '+allskyGti
+            raise
+        header=hdu[0].header
+        
+        filter['tmin']=header['TSTART']
+        filter['tmax']=header['TSTOP']
+        filter['zmax']=100.
+        filter['evclass']=2
+        filter.run()
+
+
+    def makeTime(src):
+        """
+        Filter the GTI for a given source
+        """
+        print "Not yet implemented"
+        
+
+
+    def computePhotometricLC(src):
+        """
+        Compute the photometric light curve for a given source
+        """
         print "Not yet implemented"
 
 
@@ -67,7 +121,10 @@ def main(argv=None):
     else:
         auto=autoLC()
 
-    auto.readSourceList()
+    srcLst=auto.readSourceList()
+    src1=srcLst[0]
+    auto.selectSrc(zip(*src1))
+    
 
 
 if __name__ == '__main__':
