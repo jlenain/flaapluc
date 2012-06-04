79bbc0797bb484060a52ce6e0937dddee28e6506
jplenain <jplenain@96316d67-cb15-4e38-b937-3d89b22596c3>
2012-06-04 08:33:53 +0000

    Test SVN keywords.

--- automaticLightCurve/automaticLightCurve.py
+++ automaticLightCurve/automaticLightCurve.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 #
-# @author Jean-Philippe Lenain, jplenain@lsw.uni-heidelberg.de
-# @version $Id$
+# @author $Author:$ 
+# Jean-Philippe Lenain, jplenain@lsw.uni-heidelberg.de
+# @version $Id:$
 
 """
 Automatic generation of photometric light curves of Fermi sources.
