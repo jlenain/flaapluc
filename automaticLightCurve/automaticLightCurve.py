976d1290e3d24ba61798f28e4a8227d3133c4a32
jplenain <jplenain@96316d67-cb15-4e38-b937-3d89b22596c3>
2012-06-04 08:47:51 +0000

    - Implemented reader for list of sources. - Structure of program put in place.

--- automaticLightCurve/automaticLightCurve.py
+++ automaticLightCurve/automaticLightCurve.py
@@ -8,3 +8,54 @@
 Automatic generation of photometric light curves of Fermi sources.
 """
 
+import sys, os, asciidata
+
+def readSourceList(file="/home/jplenain/fermi/automaticLightSource/listSources.txt"):
+    try:
+        import asciidata
+    except ImportError:
+        print "ERROR Can't import asciidata, needed to read the list of sources. Aborting..."
+        sys.exit(1)
+
+    try:
+        srcList=asciidata.open(file)
+    except IOError:
+        print "ERROR Can't open "+file
+        sys.exit(1)
+
+    src = srcList[0]
+    ra  = srcList[1]
+    dec = srcList[2]
+    z   = srcList[3]
+    
+    if DEBUG:
+        print "DEBUG src="+src[1]
+        print "DEBUG ra ="+ra[1]
+        print "DEBUG dec="+dec[1]
+        print "DEBUG z  ="+z[1]
+
+
+
+
+def main(argv=None):
+    """
+    Main procedure
+    """
+
+    argc    = len(sys.argv)
+    argList = sys.argv
+    
+    if(argc<2):
+        file=argList[0]
+        print "Overriding default list of source: using "+file
+        readSourceList(file)
+    else:
+        readSourceList
+
+
+if __name__ == '__main__':
+    """
+    Execute main()
+    """
+
+    main()
