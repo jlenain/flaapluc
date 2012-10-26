d7a44821b8d5cffe89a92c02437cda3767e8c75f
fermi <fermi@96316d67-cb15-4e38-b937-3d89b22596c3>
2012-10-26 10:59:50 +0000

    - Changed zmax from 105 deg to 100 deg. - Changed emax from 100 GeV to 300 GeV.

--- automaticLightCurve/automaticLightCurve.py
+++ automaticLightCurve/automaticLightCurve.py
@@ -165,8 +165,8 @@ class autoLC:
         # Setting default parameters
         self.roi  = 1.   # degrees (http://fermi.gsfc.nasa.gov/ssc/data/analysis/scitools/aperture_photometry.html: "For aperture photometry we select a very small aperture (rad=1 degree), because we are not fitting the background.")
         self.emin = 1.e2 # E min
-        self.emax = 1.e5 # E max
-        self.zmax = 105. # degrees
+        self.emax = 3.e5 # E max
+        self.zmax = 100. # degrees
         self.daily=daily
 
         if daily:
